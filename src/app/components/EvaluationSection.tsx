01f8f14a71596804f2f1d89feb35cf3507a65e0b
Siroros Roongdonsai <siroros_okdaika@hotmail.com>
2026-03-31 13:45:14 +0700

    revert to stable commit

--- src/app/components/EvaluationSection.tsx
+++ src/app/components/EvaluationSection.tsx
@@ -50,13 +50,22 @@ export function EvaluationSection({ language }: EvaluationSectionProps) {
 useEffect(() => {
   const fetchGoldPrice = async () => {
     try {
-      const res = await fetch("https://moneybag-api.vercel.app/api/gold");
+      const API_URL =
+        import.meta.env.DEV
+          ? "/gold-api/details?readjson=false" // ✅ local (vite proxy)
+          : "https://api.allorigins.win/raw?url=https://goldtraders.or.th/api/GoldPrices/details?readjson=false"; // ✅ production
+
+      const res = await fetch(API_URL);
       const data = await res.json();
 
+      const latest = data.reduce((prev: any, current: any) =>
+        prev.goldPriceID > current.goldPriceID ? prev : current
+      );
+
       setGoldPrice({
-        buy: data.buy,
-        sell: data.sell,
-        timestamp: data.timestamp,
+        buy: latest.bL_BuyPrice,
+        sell: latest.bL_SellPrice,
+        timestamp: latest.asTime,
       });
     } catch (error) {
       console.error("Failed to fetch gold price:", error);
@@ -65,7 +74,8 @@ useEffect(() => {
 
   fetchGoldPrice();
 
-  // refresh every 5 minutes
+
+  // refresh every 60 minutes
   const interval = setInterval(fetchGoldPrice, 300000);
 
   return () => clearInterval(interval);
